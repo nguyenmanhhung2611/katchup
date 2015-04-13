5e0de459d8a807203b7faf2a321bbe526c305651
Hung Nguyen <hmnguyen@opswat.com>
2015-04-13 17:28:30 +0700

    hmnguyen: fix bug deploy

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -3,8 +3,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends CI_Controller {
 	
+	// Index controller
 	public function index() {
 		$this->load->view('index');
 	}
+	
 }
 ?>
