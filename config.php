91f5f0fc3396a4015c109ec45a95631da073b98e
Thai Chu <thaick9x@gmail.com>
2015-04-28 16:06:33 +0700

    tc-another-port: Bit modified to use another port instead of :80

--- config.php
+++ config.php
@@ -1,3 +1,11 @@
 <?php
-define('ITQ_BASE_URL', 'http://'.$_SERVER['SERVER_NAME'].'/katchup/');
+$path = $_SERVER['DOCUMENT_ROOT']."/katchup-config-port.txt";
+$port = "";
+if (file_exists($path))
+{
+	$port = file_get_contents($path);
+}
+
+define('PORT', $port);
+define('ITQ_BASE_URL', 'http://'.$_SERVER['SERVER_NAME'].PORT.'/katchup/');
 ?>
