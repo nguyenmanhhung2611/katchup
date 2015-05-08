cf8d5c0c82ef4b8cc66f5d0c4131f04781444470
Hung Nguyen <hmnguyen@opswat.com>
2015-05-08 16:49:03 +0700

    add like, share button and fix others bug

--- application/views/include/header.php
+++ application/views/include/header.php
@@ -1,4 +1,13 @@
-<div id="header" >
+<div id="header" >  
+    <div id="fb-root"></div>
+    <script>(function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v2.3";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
+    
     <div class="container noselect" id="toppage">        
       <!-- Static navbar -->
       <nav class="navbar navbar-default">
