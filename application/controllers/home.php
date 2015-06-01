343521b0cb6b7e9c0b9096ae4ad924f79b81ad74
Hung Nguyen <hmnguyen@opswat.com>
2015-06-01 20:07:21 +0700

    hmnguyen-checkout: seconds commit back-end

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -22,7 +22,10 @@ class Home extends CI_Controller {
 	}
 
 	public function product() {
-		$this->load->view('product');
+		$this->load->model('Product');
+		$data['products'] = $this->Product->getList();
+
+		$this->load->view('product', $data);
 		
 		// http://stackoverflow.com/questions/2038755/how-to-put-a-dot-after-three-digits-of-a-number
 		// echo number_format(1254631 , 0, ',', '.');  // prints 1.254.631
---
+++ application/models/Product.php
@@ -0,0 +1,79 @@
+
+<!-- 
+MaSP (int) (auto_increment)
+TenSP (varChar _ 255)
+MoTa (Text)
+GiaCu (int)
+GiaMoi (int)
+TenHinh (varchar _ 255) 
+-->
+
+<?php
+class Product extends CI_Model {
+	
+    function __construct()
+    {        
+        parent::__construct(); // Call the Model constructor
+    }
+
+	public function getList() {
+		return $this->db->select('*')->from('sanpham')->order_by('MaSP asc, TenSP asc')->get()->result_array();
+	}
+
+    public function add($name, $des, $oldPrice, $newPrice, $NameImg) {
+        try {
+            $data = array(
+                'TenSP' => $name,
+                'MoTa' => $des,
+                'GiaCu' => $oldPrice,
+                'GiaMoi' => $newPrice,
+                'TenHinh' => $NameImg
+            );
+            $this->db->insert('sanpham', $data);
+
+        } catch (Exception $e) {
+            // echo $e->getMessage();
+            return false;
+        }
+        return true;
+    }
+
+    public function edit($id, $name, $des, $oldPrice, $newPrice, $NameImg) {
+        try {
+            $id = (int)$id;
+            $data = array(
+                'MaSP' => $id,
+                'TenSP' => $name,
+                'MoTa' => $des,
+                'GiaCu' => $oldPrice,
+                'GiaMoi' => $newPrice,
+                'TenHinh' => $NameImg
+            );
+
+            $this->db->where('MaSP', $id);
+            $this->db->update('sanpham', $data);
+
+        } catch (Exception $e) {
+            // echo $e->getMessage();
+            return false;
+        }
+        return true;
+    }
+
+    public function delete($id) {
+        try {
+            $id = (int)$id;
+            // $query['sanpham'] = $this->db->select('*')->from('sanpham')->where(array('MaSP' => $id))->get()->row_array();
+            $this->db->delete('sanpham', array('id' => $id));
+
+        } catch (Exception $e) {
+            // echo $e->getMessage();
+            return false;
+        }
+        return true;
+    }
+
+
+}
+
+?>
