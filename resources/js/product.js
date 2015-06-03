c070a4679f07108562298226c1f8d1870fdf42ce
Hung Nguyen <hmnguyen@opswat.com>
2015-06-03 11:55:27 +0700

    hmnguyen-checkout: Finish page product front-end, back-end

--- resources/js/product.js
+++ resources/js/product.js
@@ -10,6 +10,7 @@ myProduct.controller("itemProductCtrl", function($scope){
 			if($PRODUCTS[i].id == id)
 				$PRODUCTS.splice(i, 1); 
 		}
+		sessionStorage.setItem("products", JSON.stringify($PRODUCTS));
 		calcTotalCheckout();
 
 		// Exists item product
@@ -26,6 +27,7 @@ myProduct.controller("itemProductCtrl", function($scope){
 	        	$PRODUCTS[i].amount = parseInt($("#" + id + " input").val());
 	        }
 	    }
+	    sessionStorage.setItem("products", JSON.stringify($PRODUCTS));
 	    calcTotalCheckout();
     };
 });
@@ -43,10 +45,8 @@ myProduct.controller("ProductCtrl", function($scope){
 			  price : productElement.find(".price").text()
 			}
 			$PRODUCTS.push(product);
-			// localStorage.setItem("products", $PRODUCTS);
-		}
-
-		
+			sessionStorage.setItem("products", JSON.stringify($PRODUCTS));
+		}		
 
 		var scope = angular.element($("#collapseCheckout")).scope();
 		setTimeout(function () {
@@ -69,8 +69,34 @@ var $PRODUCTS = [];
 // Document is ready
 $(function() {	
 	menuActiveDocument();
-	checkout();	
+	checkout();
+	addCommasProducts();
 });
+
+function chooseCity() {
+	if($( "#tphcmCity" ).is(":checked")) {
+		processChooseCity(true);
+	} else {
+		processChooseCity(false);
+	}
+	$( "#tphcmCity" ).click(function() {
+		processChooseCity(true);
+	});
+	$( "#ttkCity" ).click(function() {
+		processChooseCity(false);
+	});
+}
+
+function processChooseCity(isTphcm) {
+	if(isTphcm) {
+		$("#collapseTphcm").collapse('show');
+		$("#collapseTtk").collapse('hide');	
+
+	} else {
+		$("#collapseTtk").collapse('show');
+		$("#collapseTphcm").collapse('hide');
+	}
+}
 
 function menuActiveDocument() {
 	$(".katchup-menu li").each(function(index) {
@@ -82,9 +108,26 @@ function menuActiveDocument() {
 }
 
 function checkout() {
-	/*if(localStorage.getItem("products") != null) {
-		$PRODUCTS = localStorage.getItem("products");
-	}*/
+	if(sessionStorage.getItem("products") != null) {
+		$PRODUCTS = JSON.parse(sessionStorage.getItem('products'));
+
+		var scope = angular.element($("#collapseCheckout")).scope();
+		setTimeout(function () {
+	        scope.$apply(function() {
+				scope.products = $PRODUCTS;
+			});
+			calcTotalCheckout();
+
+			// Exists item product
+			if($(".checkout-item").length) {
+				$('#collapseCheckout').collapse('show');
+			}
+	    }, 500);
+	}
+
+	chooseCity();
+	validateFormCheckout();
+
 	$('#collapseCheckout').on('show.bs.collapse', function () {
 		// Exists item product
 		if(!$(".checkout-item").length) {
@@ -102,12 +145,147 @@ function checkout() {
 	});
 }
 
+function validateFormCheckout() {
+	$("#name-checkout").blur(function() {
+		if($(this).val().trim() == "") {
+			cssCheckErrorClass($(this), true);
+		} else {
+			cssCheckErrorClass($(this), false);
+		}
+	});
+
+	$("#address-checkout").blur(function() {
+		if($(this).val().trim() == "") {
+			cssCheckErrorClass($(this), true);
+		} else {
+			cssCheckErrorClass($(this), false);
+		}
+	});
+
+	$("#phone-checkout").blur(function() {
+		if(!validatePhoneNumber($(this).val())) {
+			cssCheckErrorClass($(this), true);
+		} else {
+			cssCheckErrorClass($(this), false);
+		}
+	});
+
+	$("#checkout_form").submit(function(e){
+		var hasError = !validatePhoneNumber($("#phone-checkout").val()) || $("#name-checkout").val().trim() == "" || $("#address-checkout").val().trim() == "";		
+		if(hasError) {
+			// name-checkout
+			if($("#name-checkout").val().trim() == "") {
+				cssCheckErrorClass($("#name-checkout"), true);
+			} else {
+				cssCheckErrorClass($("#name-checkout"), false);
+			}
+
+			// address-checkout
+			if($("#address-checkout").val().trim() == "") {
+				cssCheckErrorClass($("#address-checkout"), true);
+			} else {
+				cssCheckErrorClass($("#address-checkout"), false);
+			}
+
+			// phone-checkout
+			if(!validatePhoneNumber($("#phone-checkout").val())) {
+				cssCheckErrorClass($("#phone-checkout"), true);
+			} else {
+				cssCheckErrorClass($("#phone-checkout"), false);
+			}
+			return false;
+
+		} else {
+			$('#modalAlert').modal('show');
+			$("#title-Modal-Alert").text("Thông tin");
+			$("#content-Modal-Alert").html("Cám ơn bạn đã mua sản phẩm của chúng tôi </br> Đơn hàng của bạn đã được gửi đi, chúng tôi sẽ liên lạc với bạn trong thời gian sớm nhất.");
+			$('#btnClose-Modal-Alert').click(function() {
+				AjaxCheckout();
+			});
+			return false;
+		}		
+	});
+}
+
+function AjaxCheckout() {
+	var products = [];	
+	$(".checkout-item").each(function() {
+	    var product = {
+		  id : $(this).attr("id"),
+		  title : $(this).find(".title").text(),
+		  price : removeCommas($(this).find(".price b").text()),
+		  amount : $(this).find("input").val()
+		}
+		products.push(product);
+	});
+
+	var city = "ttk";
+	var optionCity = $(".ttk-detail span").text();
+	if($( "#tphcmCity" ).is(":checked")) {
+		city = "tphcm";
+		$("#collapseTphcm .radio input").each(function() {
+		  if($(this).is(":checked")) {
+		    optionCity = $(this).parent().text();
+		  }
+		});
+	}	
+
+	var postData = {
+		'city' : city,
+		'optionCity' : optionCity,
+		'product' : products,
+		'total' : removeCommas($("#total").text()),
+    	'name' : $("#name-checkout").val().trim(),
+    	'address' : $("#address-checkout").val(),
+    	'phone' : $("#phone-checkout").val().trim(),
+    	'email' : $("#email-checkout").val().trim()
+    };
+
+	$.ajax ({
+		url : $PN + "SanPham/thanh_toan",
+		type : "post",
+		// dataType : "json",
+		cache: false,
+		// async: false,
+		data : postData,
+		timeout : $AJAX_TIMEOUT,
+		success : function(data) {
+			// console.log(data);
+			if(data != 'Error') {
+				sessionStorage.removeItem("products");
+				location.reload();
+			}			
+		},
+		error : function(x, t, m) {
+			alert($ERR_BUSY);
+		},		
+	});
+}
+
 function calcTotalCheckout() {
 	var total = 0;
 	$(".checkout-item").each(function() {
 		total += parseInt(removeCommas($(this).find(".price b").text())) * parseInt($(this).find("input").val());
 	});
 	$(".checkout-total #total").text(commas(total.toString()) + "đ");
+}
+
+function addCommasProducts() {
+	$(".product-item .caption").each(function() {
+		if($(this).find(".price-old").text() == "0đ") {
+			$(this).find(".price-old").text("00.000đ");
+			$(this).find(".price").text("00.000đ");
+		} else {
+			$(this).find(".price-old").text(commasItemProduct($(this).find(".price-old").text()));
+			$(this).find(".price").text(commasItemProduct($(this).find(".price").text()));
+		}		
+	});
+}
+
+// Add commas to item product
+function commasItemProduct(str) {
+	str = str.replace("đ", "");
+    return str.replace(/.(?=(?:.{3})+$)/g, '$&.') + "đ";
 }
 
 // Add commas in price
@@ -117,7 +295,7 @@ function commas(str) {
 
 // Remove commas in price
 function removeCommas(str) {
-	str.replace("đ", "");
+	str = str.replace("đ", "");
     return str.replace(/\./g, "");
 }
 
@@ -131,6 +309,7 @@ function checkExistsproduct(id) {
 	for (i = 0; i < $PRODUCTS.length; i++) {
         if($PRODUCTS[i].id == id) {
         	$PRODUCTS[i].amount = $PRODUCTS[i].amount + 1;
+        	sessionStorage.setItem("products", JSON.stringify($PRODUCTS));
         	return true;
         }
     }
