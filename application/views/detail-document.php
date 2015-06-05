134d2fbb48e88f4998df546d5bd44d62586392d2
Thai Chu <thaick9x@gmail.com>
2015-06-05 08:27:50 +0700

    thai-chu: test FB comment

--- application/views/detail-document.php
+++ application/views/detail-document.php
@@ -209,13 +209,7 @@
             <div class="row facebook-comment">
               <div class="col-xs-12 col-sm-12">
                 <div id="fb-root"></div>
-                <script>(function(d, s, id) {
-                  var js, fjs = d.getElementsByTagName(s)[0];
-                  if (d.getElementById(id)) return;
-                  js = d.createElement(s); js.id = id;
-                  js.src = "//connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v2.3";
-                  fjs.parentNode.insertBefore(js, fjs);
-                }(document, 'script', 'facebook-jssdk'));</script>
+                
                 <div class="fb-comments" data-href="http://localhost/katchup/home/detailDocument/1" data-numposts="10" data-colorscheme="light"></div>
               </div>
             </div>
